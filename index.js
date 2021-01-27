6ebf2c3f1b88d90d89765613a954beafddc7da8b
Agraj <agrajgarg1509@gmail.com>
2021-01-27 10:52:47 +0530

    Delete index.html

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const ejs=require("ejs");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
